e816a1872187fb7860f1a3014e9b683bf287a776
ionutrogojan <i.rogojan15@gmail.com>
2024-03-30 21:09:36 +0000

    fixed logging error and added error logging for commands

--- discoed.sh
+++ discoed.sh
@@ -9,50 +9,62 @@ DISCORD_BUILD_INFO="$INSTALL_PATH/Discord/resources/build_info.json"
 DISCORD_API="https://discord.com/api/updates/$DISCORD_BRANCH?platform=linux"
 DISCORD_DOWNLOAD="https://discord.com/api/download?platform=linux&format=tar.gz"
 DESKTOP_LINKS_PATH="$HOME/.local/share/applications"
+DESKTOP_FILE_PATH="$DESKTOP_LINKS_PATH/discoed.desktop"
 CWD=$(dirname $(realpath "$0"))
+LOG_FILE_PATH="$CWD/discoed.log"
 
 log_data() {
 	DATA="$1"
 	NOW=$(date "+%d-%m-%Y %H:%M")
-	printf "[%s] %s\n" "$NOW" "$DATA" >> "$CWD/discoed.log"
+	printf "[%s] %s\n" "$NOW" "$DATA" >> "$LOG_FILE_PATH"
+}
+
+warn_error() {
+	ERROR="$1"
+	printf "\33[1;35m[ERROR]\33[0m %s\n" "$ERROR"
+	log_data "<ERROR> $ERROR"
+	xdg-open "$LOG_FILE_PATH"
+	exit 1
 }
 
 install_discord() {
 	if [ ! -d "$DOWNLOADS_PATH" ]; then
 		ERROR="\$DOWNLOADS_PATH does not exist at the specified path"
-		printf "\33[1;35m[ERROR]\33[0m %s\n" "$ERROR"
-		log_data "<ERROR> $ERROR"
-		xdg-open "$CWD/discoed.log"
 		xdg-open "$0"
-		exit 1
+		warn_error "$ERROR"
 	fi
 	if [ ! -d "$INSTALL_PATH" ]; then
 		ERROR="\$INSTALL_PATH does not exist at the specified path"
-		printf "\33[1;35m[ERROR]\33[0m %s\n" "$ERROR"
-		log_data "<ERROR> $ERROR"
-		xdg-open "$CWD/discoed.log"
 		xdg-open "$0"
-		exit 1
+		warn_error "$ERROR"
 	fi
-# TODO: check if all of these exit succesfully and log the errors if not
 # ----> Download Discord
 	DISCORD_TAR="$DOWNLOADS_PATH/discord-$EXTRN_VERSION.tar.gz"
 	wget -O $DISCORD_TAR $DISCORD_DOWNLOAD
+	if [ ! $? -eq 0 ]; then
+		ERROR="wget failed to retrieve discord tarball"
+		warn_error "$ERROR"
+	fi
 # ----> Extract & Replace
 	tar -xzf $DISCORD_TAR --overwrite -C $INSTALL_PATH
+	if [ ! $? -eq 0 ]; then
+		ERROR="tar failed to extract or replace the contents at the specified path"
+		warn_error "$ERROR"
+	fi
 # ----> Remove Tar
 	rm -rf $DISCORD_TAR
+	if [ ! $? -eq 0 ]; then
+		ERROR="rm failed to remove the temporary tar file downloaded"
+		warn_error "$ERROR"
+	fi
 	log_data "<INFO> Discord updated to the latest version $EXTRN_VERSION"
 }
 
 run_discord() {
 	if [ ! -e "$INSTALL_PATH/Discord/Discord" ]; then
 		ERROR="Discord executable does not exist at the specified path"
-		printf "\33[1;35m[ERROR]\33[0m %s\n" "$ERROR"
-		log_data "<ERROR> $ERROR"
-		xdg-open "$CWD/discoed.log"
 		xdg-open "$0"
-		exit 1
+		warn_error "$ERROR"
 	fi
 # ----> Run Discord
 	"$INSTALL_PATH/Discord/Discord" &
@@ -60,24 +72,27 @@ run_discord() {
 }
 
 # ----> Find Desktop Link
-if [ ! -f "$DESKTOP_LINKS_PATH/discoed.desktop" ]; then
+if [ ! -f "$DESKTOP_FILE_PATH" ]; then
 	MESSAGE="Missing 'discoed.desktop' link from quick launcher"
 	printf "[INFO] %s\n" "$MESSAGE"
 	log_data "<INFO> $MESSAGE"
 # ----> Copy Desktop Link
 	cp "$CWD/discoed.desktop" "$DESKTOP_LINKS_PATH"
+	if [ ! $? -eq 0 ]; then
+		ERROR="cp failed to copy the 'discoed.desktop' file to its destination path"
+		warn_error "$ERROR"
+	fi
 # ----> Check Path	
 	if [ ! -f "$DESKTOP_LINKS_PATH/discoed.desktop" ]; then
-		log_data "<ERROR> Falied to create desktop link"
-		xdg-open "$CWD/discoed.log"
-		exit 1
+		ERROR="Falied to create desktop link"
+		warn_error "$ERROR"
 	fi
 	MESSAGE="Desktop link created successfully"
 	printf "[INFO] %s\n" "$MESSAGE"
-	log_data "<INFO> %s" "$MESSAGE"
+	log_data "<INFO> $MESSAGE"
 # ----> Update Desktop Link
-	sed -i "s|{discoed.sh}|$CWD/discoed.sh|g" "$DESKTOP_LINKS_PATH/discoed.desktop"
-	sed -i "s|{discoed.png}|$CWD/discoed.png|g" "$DESKTOP_LINKS_PATH/discoed.desktop"
+	sed -i "s|{discoed.sh}|$CWD/discoed.sh|g" "$DESKTOP_FILE_PATH"
+	sed -i "s|{discoed.png}|$CWD/discoed.png|g" "$DESKTOP_FILE_PATH"
 fi
 
 # ----> Web Server Query
